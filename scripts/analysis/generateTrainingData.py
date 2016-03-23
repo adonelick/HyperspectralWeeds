6650431e3f9526c622f5d236bef0a69e08a4ffc0
adonelick <adonelick@hmc.edu>
2016-03-23 13:58:15 -0600

    Added ability to delete old training data when generating new training data

--- scripts/analysis/generateTrainingData.py
+++ scripts/analysis/generateTrainingData.py
@@ -15,7 +15,7 @@ from common.Constants import *
 from common import DataManipulation
 
 
-def main(date, keywords=[]):
+def main(date, delete, keywords=[]):
     """
     Generates ML training and testing data from extracted CSV files
 
@@ -29,8 +29,27 @@ def main(date, keywords=[]):
     dataPath = DATA_DIRECTORIES[date]
     dataFilenames = FileIO.getDatafileNames(dataPath, keywords)
 
+    # If desired, remove the old training data and start fresh
+    if delete:
+
+        mlDataPath = DATA_DIRECTORIES[date+"_ML"]
+        trainingDataPath = os.path.join(mlDataPath, TRAINING_DATA_PATH)
+        testingDataPath = os.path.join(mlDataPath, TESTING_DATA_PATH)
+        sampleCountsPath = os.path.join(mlDataPath, SAMPLE_COUNTS_PATH)
+
+        if os.path.exists(trainingDataPath):
+            os.remove(trainingDataPath)
+
+        if os.path.exists(testingDataPath):
+            os.remove(testingDataPath)
+
+        if os.path.exists(sampleCountsPath):
+            os.remove(sampleCountsPath)
+
+    # Consolidate the CSV files into training and testing data
     (train_X, train_y, test_X, test_y) = DataManipulation.separateTrainTest(dataPath, dataFilenames, byLeaf=True)
 
+    # Save the training and testing data in the proper spot
     FileIO.saveTrainingData(date, train_X, train_y)
     FileIO.saveTestingData(date, test_X, test_y)
 
@@ -39,11 +58,13 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generate ML model training data from CSV files')
     parser.add_argument('date', type=str, nargs=1,
                          help='Data collection date YYYY_MMDD')
+    parser.add_argument('-d', '--delete', default=False, action='store_true',
+                         help="Delete previous training/testing data")
     parser.add_argument('-k', '--keywords', default=[], type=str, nargs='*',
                          help="Filename keywords to include in the data")
 
     args = parser.parse_args()
-    main(args.date[0], args.keywords)
+    main(args.date[0], args.delete, args.keywords)
 
 
 
